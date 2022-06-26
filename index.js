c1ae2df6ebe37352eb3d7d5db2add96d2531444c
vanshbajaj13 <vanshbajaj705@gmail.com>
2022-06-27 01:44:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// auto typing heading
+var txt = "DRUM KIT";
+var i = 0;
+function autotype() {
+  if (i < txt.length) {
+    document.querySelector("header").innerHTML += txt.charAt(i);
+    i++;
+  }
+  else{
+    document.querySelector("header").innerHTML = "";
+    i = 0;
+  }
+}
+
+setInterval(autotype,300);
+
+
+
+
+// by buttons
+var drum = document.getElementsByClassName("items");
+for (let i = 0; i < drum.length; i++) {
+  drum[i].addEventListener("click", () => {
+    var dr = drum[i].innerText;
+    console.log(dr);
+    drum[i].style.color = "red";
+    play(dr);
+  });
+}
+
+// by keys
+document.addEventListener("keydown", function (evnt) {
+  var aud;
+  play(evnt.key);
+});
+
+// function
+function play(key) {
+  var aud;
+  switch (key) {
+    case "w":
+      aud = new Audio("/ud2/sounds/crash.mp3");
+      aud.play();
+      break;
+
+    case "a":
+      aud = new Audio("/ud2/sounds/tom-3.mp3");
+      aud.play();
+      break;
+
+    case "s":
+      aud = new Audio("/ud2/sounds/kick-bass.mp3");
+      aud.play();
+      break;
+
+    case "d":
+      aud = new Audio("/ud2/sounds/tom-1.mp3");
+      aud.play();
+      break;
+
+    case "j":
+      aud = new Audio("/ud2/sounds/tom-2.mp3");
+      aud.play();
+      break;
+
+    case "k":
+      aud = new Audio("/ud2/sounds/snare.mp3");
+      aud.play();
+      break;
+
+    case "l":
+      aud = new Audio("/ud2/sounds/tom-4.mp3");
+      aud.play();
+      break;
+
+    default:
+      console.log(evnt);
+      break;
+  }
+}
